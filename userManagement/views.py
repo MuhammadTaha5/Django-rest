16f7a197547c2721ae9a64e6e533b8ff8236e065
MuhammadTaha5 <taha.saeed339@gmail.com>
2026-03-30 17:49:25 +0500

    Add Comments

--- userManagement/views.py
+++ userManagement/views.py
@@ -11,38 +11,54 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 class login(APIView):
     permission_classes = [AllowAny]
+    #allow all users to access the login url
+    #post method to send login credential
     def post(self, request):
-        reqData = request.data
+        reqData = request.data  #to extract payload from post request
         userSerial = loginSerializer(data=reqData)
+        #serializing the data
+        #checking data matches with the User model
         if userSerial.is_valid():
             username = userSerial.validated_data['username']
             password = userSerial.validated_data['password']
+            #authenticating the user exist in db
             userCheck = authenticate(username=username, password=password)
+
             if userCheck:
+                #Generating the jwt token
                 refresh = RefreshToken.for_user(userCheck)
+                #sending the response of access, and refresh token
                 return Response({
                 'refresh': str(refresh),
                 'access': str(refresh.access_token),
                 }, status=status.HTTP_200_OK)
             
+            #if authentication fails, send login fail response
             return Response({
                 'status': 'Login Failed',
                 'message': 'Incorrect Fields'   
             }, status=status.HTTP_401_UNAUTHORIZED)
         else:
+            #returning response if request data doesnt match with login serializer
             return Response({
                 'status': 'Failed',
                 'message': userSerial.errors
             }, status=status.HTTP_401_UNAUTHORIZED)
 
-
+#creating class based views
 class users(APIView):
+    #only authenticated user can access these methods(get, put, delete, post)
     permission_classes = [IsAuthenticated]
+    #post method to create user
     def post(self, request):
         reqData = request.data
+        #extract the request dat from main request
         serialize = UserSerializer(data=reqData)
+        #serializing the incoming data
         if serialize.is_valid():
+            #adding the 
             serialize.save()
+
             return Response({
                 'status' : 'success',
                 'data': serialize.data
@@ -76,12 +92,14 @@ class users(APIView):
             }, status=status.HTTP_400_BAD_REQUEST)
         
 class UserDetail(APIView):
+    #check if user is authenticated or not
     permission_classes = [IsAuthenticated]
     def get(self, request, id):
         try:
             user = User.objects.get(user_id=id)  #fetching the user by Id
         except User.DoesNotExist:   ##checking if user exist
-            return Response({
+            #returning the Response if user not found
+            return Response({   
                 'status': 'failed',
                 'message': f'User with id {id} not found'
             }, status=status.HTTP_404_NOT_FOUND)
